20ff550dea1cb88cec3b5ce0ffaa594a3a1403bb
sara-p- <sarapitt88@gmail.com>
2025-06-15 14:02:42 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+function App() {
+  return (
+    <div className='home'>
+      <div className='wrapper'>
+        <div className='container'>
+          <h1 className='title'>Hey cutie</h1>
+          <h2>It's your birthday!</h2>
+          <div className='content'>
+            <p>
+              You are one of the most talented people I know, and I am
+              consistently inspired by your dedication to your goals. I have no
+              doubt that you will achieve whatever you set your mind to, and I
+              cannot wait to help in whatever way I can.
+            </p>
+            <p>
+              I don’t have very many straight friends (for various reasons), but
+              you are such a kind, generous, and nonjudgmental person that I
+              consider myself lucky to have you as one of my closest friends.
+              You make me feel safe.
+            </p>
+            <p>
+              Your work is incredible. I wish I had a billion dollars so I could
+              buy all of it. Ever since glass class I’ve been forever impressed
+              by your blending of technique and concept. You have such a strong
+              artistic voice that’s really apparent in everything you make, in
+              the best way possible. I cannot wait to see how your work will
+              continue to evolve.
+            </p>
+            <p>
+              You are so important to me Stephen. I am so thankful that you and
+              I have gotten so much closer over the past few years. You are an
+              absolutely gem of a person and we are all lucky to have you.
+            </p>
+            <p>I love you, and hope you have an incredible birthday.</p>
+            <p>Love,</p>
+            <p className='signature'>SP</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
